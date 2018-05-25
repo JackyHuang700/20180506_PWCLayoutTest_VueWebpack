11ce04e929b0cc1bc5c8e470ed08ab863f3bb0ea
Jacky <jacky8595125@gmail.com>
2018-05-25 11:16:09 +0800

    fix 編輯收款單

--- src/components/Collectionbalance2.vue
+++ src/components/Collectionbalance2.vue
@@ -107,11 +107,63 @@
           </div>
           <div class="card-body">
 
-            <div class="form-row">
-              <div class="form-group col-sm-3">
-              </div>
-
-            </div>
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">沖帳明細</th>
+                  <th scope="col">銷售單號</th>
+                  <th scope="col" class="d-none d-sm-table-cell">幣種</th>
+                  <th scope="col">應付金額</th>
+                  <th scope="col">訂單沖帳金額</th>
+                  <th scope="col">收款編號</th>
+                  <th scope="col" class="d-none d-sm-table-cell">幣種</th>
+                  <th scope="col">收款金額</th>
+                  <th scope="col">收款單沖帳金額</th>
+                  <th scope="col" class="d-none d-md-table-cell">沖帳人</th>
+                  <th scope="col" class="d-none d-md-table-cell">沖帳時間</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <a href="//" target="_blank">-02</a>
+                  </td>
+                  <td>
+                    <a href="//" target="_blank">1801010003</a>
+                  </td>
+                  <td class="d-none d-sm-table-cell">美元</td>
+                  <td>116.78</td>
+                  <td>3.31</td>
+                  <td>
+                     <a href="//" target="_blank">2171128001</a>
+                  </td>
+                  <td class="d-none d-sm-table-cell">菲律宾比索</td>
+                  <td>123,456.00</td>
+                  <td>100.00</td>
+                  <td class="d-none d-md-table-cell">業務測試</td>
+                  <td class="d-none d-md-table-cell">2018-01-13 15:15:18</td>
+                </tr>
+                <tr>
+                  <td>
+                    <a href="//" target="_blank">2171128001-01</a>
+                  </td>
+                  <td>
+                    <a href="//" target="_blank">1712080011</a>
+                  </td>
+                  <td class="d-none d-sm-table-cell">日圓</td>
+                  <td>24,341.90</td>
+                  <td>10,000.00</td>
+                  <td>
+                     <a href="//" target="_blank">2171128001</a>
+                  </td>
+                  <td class="d-none d-sm-table-cell"> 菲律賓比索</td>
+                  <td>123,456.00</td>
+                  <td>100,000.00</td>
+                  <td class="d-none d-md-table-cell">網站管理員</td>
+                  <td class="d-none d-md-table-cell">2017-12-08 15:31:09</td>
+                </tr>
+              </tbody>
+            </table>
 
           </div>
         </div>
@@ -133,5 +185,4 @@ export default {
 }
 </script>
 <style lang="css" >
-
 </style>
