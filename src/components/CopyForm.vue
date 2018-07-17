7dd90caaaa86b66598754f5ef009cea7bf8c3966
Jacky <jacky8595125@gmail.com>
2018-07-17 17:24:09 +0800

    fix copyform

--- src/components/CopyForm.vue
+++ src/components/CopyForm.vue
@@ -50,11 +50,84 @@
           <div class="card-footer"></div>
         </div>
       </div>
+
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <i class="fa fa-wpforms" aria-hidden="true"></i>表單2
+          </div>
+          <div class="card-body">
+            <div class="form-group row">
+              <label for="template6" class="col-sm-2 col-form-label">...</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="template6" placeholder="請填寫...">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="template7" class="col-sm-2 col-form-label">...</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="template7" placeholder="請填寫...">
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <label for="template9" class="col-sm-2 col-form-label">...</label>
+              <div class="col-sm-4">
+                <input type="text" class="form-control" id="template9" placeholder="請填寫...">
+              </div>
+              <label for="template10" class="col-sm-2 col-form-label">...</label>
+              <div class="col-sm-4">
+                <input type="text" class="form-control" id="template10" placeholder="請填寫...">
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <label for="template11" class="col-sm-1 col-form-label">...</label>
+              <div class="col-sm-3">
+                <input type="text" class="form-control" id="template11" placeholder="請填寫...">
+              </div>
+              <label for="template12" class="col-sm-1 col-form-label">...</label>
+              <div class="col-sm-3">
+                <input type="text" class="form-control" id="template12" placeholder="請填寫...">
+              </div>
+              <label for="template13" class="col-sm-1 col-form-label">...</label>
+              <div class="col-sm-3">
+                <input type="text" class="form-control" id="template13" placeholder="請填寫...">
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <label for="template8" class="col-sm-2 col-form-label">...</label>
+              <div class="col-sm-10">
+                <select id="template8" name="template8" class="form-control">
+                  <option selected>所有</option>
+                  <option>...</option>
+                  <option>...</option>
+                </select>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col-sm-7">
+                <input type="text" class="form-control" placeholder="City">
+              </div>
+              <div class="col-sm">
+                <input type="text" class="form-control" placeholder="State">
+              </div>
+              <div class="col-sm">
+                <input type="text" class="form-control" placeholder="Zip">
+              </div>
+            </div>
+          </div>
+          <div class="card-footer"></div>
+        </div>
+      </div>
+
       <div class="col-12 text-right">
         <button type="submit" class="btn btn-success col-sm-2 mb-3">儲存</button>
         <a href="" class="btn btn-danger col-sm-1 mb-3">返回</a>
       </div>
     </div>
+
   </div>
 </template>
 <script>
